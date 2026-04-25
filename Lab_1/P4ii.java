db964ca87b545506a9ff2f116bbf052e017a13fa
Vrinda-Siddhpura <vrindasiddhpura11@gmail.com>
2026-04-26 00:08:15 +0530

    Add files via upload

---
+++ Lab_1/P4ii.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class P4ii{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter 1st Number: ");
+		int a = sc.nextInt();
+		System.out.print("Enter 2nd Number: ");
+		int b = sc.nextInt();
+		System.out.println("Sum: "+(a+b));
+	}
+}
