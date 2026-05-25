290674bd403af87405b649635d9589934278ab1d
Vrinda <vrindasiddhpura11@gmail.com>
2026-05-25 15:02:54 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class P1{
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter Size: ");
+		int size = sc.nextInt();
+		
+		int arr[] = new int[size];
+		System.out.print("Enter Elements: ");
+		for(int i=0 ; i<size ; i++){
+			arr[i] = sc.nextInt();
+		}
+
+		int sum = 0;
+		for(int i=0 ; i<size ; i++){
+			sum += arr[i];
+		}
+
+		System.out.print("Sum: " +sum);
+	}
+}
