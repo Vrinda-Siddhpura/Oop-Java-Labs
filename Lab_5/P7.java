41a51fe85d3fc2e92a472273d16d5cd953787fa6
Vrinda <vrindasiddhpura11@gmail.com>
2026-07-12 15:39:23 +0530

    Update P7.java

--- Lab_5/P7.java
+++ Lab_5/P7.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 
 public class P7{
-	public static int search(int arr[], int target){
-		int i = 0;
-		int j = arr.length-1;
+	public static void main(String []args){
+		Scanner sc = new Scanner(System.in);
+
+		int arr[] = {4,5,6,7,0,1,2};
+		int target = 0;
+
+		int i=0;
+		int j=arr.length-1;
 
 		while(i<=j){
 			int mid = (i+j)/2;
 
-			if(arr[mid]==target){
-				return mid;
+			if(target==arr[mid]){
+				System.out.print("Index: "+mid);
+				return;
 			}
 
 			if(arr[i]<=arr[mid]){
@@ -21,28 +27,15 @@ public class P7{
 				}
 			}
 			else{
-				if(arr[mid]<target && target<=arr[j]) {
-                    i = mid+1;
-                } else {
-                    j = mid-1;
-                }
+				if(arr[mid]<target && target<=arr[j]){
+					i = mid+1;
+				}
+				else{
+					j = mid-1;
+				}
 			}
-
 		}
 
-		return -1;
-	}
-
-
-	public static void main(String []args){
-		Scanner sc = new Scanner(System.in);
-
-		int arr1[] = {4,5,6,7,0,1,2};
-		int target1 = 0;
-		System.out.println("Index = " +search(arr1, target1));
-
-		int arr2[] = {4,5,6,7,0,1,2};
-		int target2 = 3;
-		System.out.println("Index = " +search(arr2, target2));
+		System.out.print("Index: -1");
 	}
 }
